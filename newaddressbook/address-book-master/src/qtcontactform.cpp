762e26cd41c7c5ca66dd86b0317c3aa09c63b72f
student <lavanyasamymsc.gmail.com>
2016-11-07 13:37:57 -0800

    After adding city and country

---
+++ newaddressbook/address-book-master/src/qtcontactform.cpp
@@ -0,0 +1,35 @@
+#include <QLabel>
+#include <QLineEdit>
+#include <QFormLayout>
+#include "qtcontactform.h"
+
+
+
+QtContactForm::QtContactForm(QWidget *parent) : 
+        QFormLayout(parent), firstNameField(new QLineEdit()),
+        lastNameField(new QLineEdit()),
+        addressField(new QLineEdit()),
+        phoneNumberField(new QLineEdit()),
+        emailField(new QLineEdit()),
+        cityField(new QLineEdit()),
+        countryField(new QLineEdit())
+{
+    addRow("First Name<font background = ''color='red'>*</font>", firstNameField);
+    addRow("Last Name<font color='red'>*</font>", lastNameField);
+    addRow("Address", addressField);
+    addRow("Phone Number<font color='red'>*</font>", phoneNumberField);
+    addRow("Email<font color='red'>*</font>", emailField);
+    addRow("city", cityField);
+    addRow("country", countryField);
+}
+
+void QtContactForm::clear()
+{
+    firstNameField->setText("");
+    lastNameField->setText("");
+    addressField->setText("");
+    phoneNumberField->setText("");
+    emailField->setText("");
+    cityField->setText("");
+    countryField->setText("");
+}
